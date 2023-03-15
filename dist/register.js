1afbb02c851d70ed4311cfb48204a2a183b579cb
Bahdree Abdullah <bahdree@aleph-labs.com>
2023-03-16 04:44:16 +0800

    completed register form

--- dist/register.js
+++ dist/register.js
@@ -1,29 +1,87 @@
-$(document).ready(function() {
-  $('input[name="option1"]').change(function() {
-    var selected = $('input[name="option1"]:checked').val();
-    if (selected) {
-      $('input[name="option2"]').prop("disabled", false).parent().show();
-      $('input[name="option2"][value="' + selected + '"]').prop("disabled", true).parent().hide();
-      $('input[name="option2"]').prop("checked", false);
-      $('input[name="option3"]').prop("disabled", true).parent().hide();
-      $('input[name="option3"]').prop("checked", false);
-    } else {
-      $('input[name="option2"]').prop("disabled", true).parent().hide();
-      $('input[name="option2"]').prop("checked", false);
-      $('input[name="option3"]').prop("disabled", true).parent().hide();
-      $('input[name="option3"]').prop("checked", false);
-    }
-  });
+const studentForm = document.getElementById('student-form');
+const studentButton = document.getElementById('student-button');
+const ic = document.getElementById('ic');
+const tahap = document.getElementById('tahap');
+const student = document.getElementById('student');
+const sekolah = document.getElementById('sekolah');
+const emel = document.getElementById('emel');
 
-  $('input[name="option2"]').change(function() {
-    var selected = $('input[name="option2"]:checked').val();
-    if (selected) {
-      $('input[name="option3"]').prop("disabled", false).parent().show();
-      $('input[name="option3"][value="' + selected + '"]').prop("disabled", true).parent().hide();
-      $('input[name="option3"]').prop("checked", false);
-    } else {
-      $('input[name="option3"]').prop("disabled", true).parent().hide();
-      $('input[name="option3"]').prop("checked", false);
-    }
-  });
-});
+
+
+
+const validateInputs = () => {
+  const icValue = ic.value.trim();
+  const tahapValue = tahap.value.trim();
+  const studentValue = student.value.trim();
+  const sekolahValue = sekolah.value.trim();
+  const emelValue = emel.value.trim();
+
+  if(icValue === '') {
+    setError(ic, 'Kad pengenalan perlu diisi');
+  }
+  else {
+    setSuccess(ic);
+  }
+  if(tahapValue === '') {
+    setError(tahap, 'Tahap perlu diisi');
+  }
+  else {
+    setSuccess(tahap);
+  }
+  if(studentValue === '') {
+    setError(student, 'Nama pelajar perlu diisi');
+  }
+  else {
+    setSuccess(student);
+  }
+  if(sekolahValue === '') {
+    setError(sekolah, 'Nama sekolah perlu diisi');
+  }
+  else {
+    setSuccess(sekolah);
+  }
+  if(emelValue === '') {
+    setError(emel, 'Emel perlu diisi');
+  }
+  else {
+    setSuccess(emel);
+  }
+}
+
+const setError = (element, message) => {
+
+  const inputControl = element.parentElement;
+  const parentInput = inputControl.parentElement;
+  const errorDisplay = inputControl.querySelector('.error-message');
+
+  errorDisplay.innerText = message;
+  parentInput.classList.add('error');
+  parentInput.classList.remove('success');
+
+}
+
+const setSuccess = (element) => {
+
+  const inputControl = element.parentElement;
+  const parentInput = inputControl.parentElement;
+  const errorDisplay = inputControl.querySelector('.error-message');
+
+  errorDisplay.innerText = '';
+  parentInput.classList.add('success');
+  parentInput.classList.remove('error');
+
+
+}
+
+
+studentForm.addEventListener('submit', function(e) {
+e.preventDefault();
+
+validateInputs();
+})
+
+
+
+
+
+
